a8311f8c6fdca836d5f562cf84f2eeea6b3088c5
alice-blue <alice-blue@users.noreply.github.com>
2024-12-20 16:59:03 -0500

    Updating file type descriptions

--- sql/incoming-schema-changes.sql
+++ sql/incoming-schema-changes.sql
@@ -7,3 +7,25 @@ alter table reviewvotes
     add column `createdate` datetime NOT NULL DEFAULT current_timestamp(),
     add PRIMARY KEY (`reviewvoteid`)
 ;
+
+
+UPDATE filetypes
+SET `desc` = 'a <a href="https://www.ifwiki.org/Hugo_interpreters">Hugo interpreter</a>' WHERE fmtname = 'Hugo';
+SET `desc` = 'a <a href="https://www.ifwiki.org/TADS_interpreters">TADS interpreter</a>' WHERE fmtname = 'TADS 2';
+SET `desc` = 'a <a href="https://www.ifwiki.org/Z-code_interpreters">Z-Code interpreter</a>' WHERE fmtname = 'Z-Code';
+SET `desc` = 'a <a href="https://www.ifwiki.org/TADS_interpreters">TADS interpreter</a>' WHERE fmtname = 'TADS 3';
+SET `desc` = 'an <a href="https://www.ifwiki.org/Alan_interpreters">Alan interpreter</a>' WHERE fmtname = 'Alan 2';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
+SET `desc` = '' WHERE fmtname = '';
