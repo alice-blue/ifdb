9b845b08b5a8662aa38c28f0ee0205cc93a904b8
alice-blue <alice-blue@users.noreply.github.com>
2026-03-14 19:38:10 +0000

    incoming schema changes: Add admin_note and sample values. adminops: start form, display info about levels. display current status of each domain.

--- sql/incoming-schema-changes.sql
+++ sql/incoming-schema-changes.sql
@@ -11,8 +11,13 @@ CREATE TABLE `suspicious_domains` (
   `mod_date` DATETIME DEFAULT now(),
   `suspicion_level` tinyint(1) UNSIGNED NOT NULL,
   `modified_by` varchar(255) NOT NULL,
+  `admin_note` varchar(255) NOT NULL,
   PRIMARY KEY (record_id)
 );
 
-insert into suspicious_domains (domain, mod_date, suspicion_level, modified_by)
-values ('gmail.com', now(), '2', '1234567ABC');  
+insert into `suspicious_domains` (domain, mod_date, suspicion_level, modified_by, admin_note)
+values ('gmail.com', '1997-01-01 00:38:54.840', '3', 'Bob', 'I really hate gmail. It should be illegal.');
+insert into suspicious_domains (domain, mod_date, suspicion_level, modified_by, admin_note)
+values ('gmail.com', '2027-12-31 08:38:54.840', '1', 'Amy', 'My latest awesome comment');  
+insert into suspicious_domains (domain, mod_date, suspicion_level, modified_by, admin_note)
+values ('outlook.com', '2020-06-20 02:38:54.840', '2', 'Dave', 'Comment goes here.'); 
